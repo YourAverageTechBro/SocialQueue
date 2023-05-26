e1b0bc1ac782de715c2de84a3a54be82fdc1fd94
YourAverageTechBro <94861246+YourAverageTechBro@users.noreply.github.com>
2023-05-26 12:19:42 -0500

    Update dashboard profile page (#7)

--- app/components/SocialAccountSetup.tsx
+++ app/components/SocialAccountSetup.tsx
@@ -12,6 +12,7 @@ import {
 import { log } from "next-axiom";
 import LoadingSpinner from "./LoadingSpinner";
 import Link from "next/link";
+import BackgroundGradient from "./BackgroundGradient";
 
 type Props = {
   setUser: Dispatch<SetStateAction<User | undefined>>;
@@ -245,38 +246,12 @@ export default function SocialAccountSetup({ loading, setUser, user }: Props) {
           height: 100%;
         }
       `}</style>
-      <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
-        <svg
-          className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
-          viewBox="0 0 1155 678"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
-            fillOpacity=".3"
-            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-          />
-          <defs>
-            <linearGradient
-              id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
-              x1="1155.49"
-              x2="-78.208"
-              y1=".177"
-              y2="474.645"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#9089FC" />
-              <stop offset={1} stopColor="#FF80B5" />
-            </linearGradient>
-          </defs>
-        </svg>
-      </div>
+      <BackgroundGradient />
       {loading && (
         <LoadingSpinner styleOverride="flex justify-center items-center w-full" />
       )}
       {!loading && !user?.notion_duplicated_template_id && (
-        <div className="flex flex-col">
+        <div className="flex flex-col w-full items-center">
           <h3 className="font-bold text-2xl">
             {" "}
             Add SocialQueue to your Notion
